f7d893abe7433ccbe22d6a3a48a4b65e7a1e8456
theogoulart <theo_goulart@hotmail.com>
2017-02-27 00:02:42 -0300

    first commit

---
+++ header.php
@@ -0,0 +1,43 @@
+<html>
+
+	<head>
+
+		<link rel="stylesheet" type="text/css" href="styles/styles.css">
+
+	</head>
+
+	<body>
+
+
+		<nav>
+
+			<div class="menu-container">
+
+				<h1>
+					theo goulart
+				</h1>
+				
+				<ul id="menu-bar">
+
+					<a href="">
+						<li class="menu-item">
+							Contact
+						</li>
+					</a>
+
+					<a href="">
+						<li class="menu-item">
+							About
+						</li>
+					</a>
+
+					<a href="">
+						<li class="menu-item">
+							Projects
+						</li>
+					</a>
+				</ul>
+
+			</div>
+
+		</nav>
---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+
+	<head>
+
+		<title>theogoulart</title>
+
+		<style type="text/css">
+
+			body {
+				font-family: helvetica;
+				margin: 0px;
+			}
+
+		</style>
+
+	</head>
+
+	<body>
+
+		<?php 
+		include("header.php");
+		include("content.php"); 
+		include("footer.php"); 
+		?>
+
+
+	</body>
+
+</html>
